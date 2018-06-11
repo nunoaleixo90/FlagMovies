0cfd5fe99c1460e21c47e68f2379c680a9c37fac
Nuno Alexandre Silva <nunoaleixosilva@gmail.com>
2018-06-11 11:46:43 +0100

    SplashScreen Update

--- app/src/main/java/pt/flag/flagmovies/screens/SplashScreen.java
+++ app/src/main/java/pt/flag/flagmovies/screens/SplashScreen.java
@@ -1,6 +1,8 @@
 package pt.flag.flagmovies.screens;
 
 
+import android.animation.Animator;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
 
@@ -16,11 +18,40 @@ public class SplashScreen extends Screen {
         setContentView(R.layout.splash_screen);
 
         findViews();
+        startAnimation();
 
     }
 
 
     public void findViews(){
         imageView = findViewById(R.id.splash_screen_imageview);
+    }
+
+    private void startAnimation(){
+        imageView.setScaleX(0.2f);
+        imageView.setScaleY(0.2f);
+        imageView.setAlpha(0.0f);
+
+        imageView.animate().alpha(1f).scaleX(1f).scaleY(1f).rotation(360).setDuration(1500).setListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                startActivity(new Intent(SplashScreen.this, HomeScreen.class));
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        })
+
     }
 }
